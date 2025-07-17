24345237e2b4e7a1dd69eaf5ddd702ce2a54c206
Kevin Raynak <kevin.raynak@dbtlabs.com>
2025-07-17 19:22:42 +0000

    Created test/assert_stg_payments_amount_is_positive.sql to see if any refund values are less than zero

---
+++ tests/assert_stg_payments_amount_is_positivie.sql
@@ -0,0 +1,16 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail.
+
+with payments as (
+    select *
+    from {{ ref('stg_stripe__payments') }}
+)
+select
+    order_id, 
+    sum(amount) as total_amount
+from
+    payment
+group by 
+    order_id
+having
+    total_amount < 0
