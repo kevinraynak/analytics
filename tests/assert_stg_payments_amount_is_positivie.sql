34bda2515c71e66aab78ac5d5207483b4870d443
Kevin Raynak <kevin.raynak@dbtlabs.com>
2025-07-17 19:23:02 +0000

    Test

--- tests/assert_stg_payments_amount_is_positivie.sql
+++ tests/assert_stg_payments_amount_is_positivie.sql
@@ -1,7 +1,7 @@
 -- Refunds have a negative amount, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail.
 
-with payments as (
+with payment as (
     select *
     from {{ ref('stg_stripe__payments') }}
 )
