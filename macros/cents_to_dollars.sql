172642018c89b0e3c162b3a17ef5c7e97441fa39
Kevin Raynak <kevin.raynak@fishtownanalytics.com>
2025-07-24 20:05:51 +0000

    Created cents_to_dollar macro. The macro rounds to two decimals places

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,3 +1,3 @@
-{% marco cents_to_dollars() %}
-amount / 100
-{% endmacro %}
+{% macro cents_to_dollars(column_name, decimal_places=2) -%}
+round( 1.0 * {{ column_name }} / 100, {{ decimal_places }})
+{%- endmacro %}
